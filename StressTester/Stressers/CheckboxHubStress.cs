1ab6a238b62979b67d5c601f15eab8884ab42e5f
Kristoffer Lindahl <kristoffer.lindahl@gmail.com>
2025-07-12 11:50:27 +0200

    Add tests to StressTester.

--- StressTester/Stressers/CheckboxHubStress.cs
+++ StressTester/Stressers/CheckboxHubStress.cs
@@ -9,16 +9,23 @@ using Microsoft.Extensions.Options;
 
 using StressTester.Options;
 
+using Two56bitId;
+
 public class CheckboxHubStressService : IHostedService
 {
+    #region Static Fields
+
+    private static readonly byte[] RandomPageId = Generate256BitHex();
+
+    #endregion
+
     #region Fields
 
+    private readonly List<Task> _checkboxHubTestTask = new();
     private readonly string _checkboxHubUrl;
     private readonly ILogger<CheckboxHubStressService> _logger;
     private readonly IOptions<CheckboxHubStressOptions> _options;
     private readonly CancellationTokenSource _testCancellationTokenSource = new();
-
-    private Task? _checkboxHubTestTask;
 
     #endregion
 
@@ -43,24 +50,34 @@ public class CheckboxHubStressService : IHostedService
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        _checkboxHubTestTask = CheckboxHubTest();
+        _checkboxHubTestTask.Add(ActivateRandomGrainsTest());
+        _checkboxHubTestTask.Add(ActivateRandomGrainsTest());
+        _checkboxHubTestTask.Add(ActivateRandomGrainsTest());
+        _checkboxHubTestTask.Add(ActivateRandomGrainsTest());
+        _checkboxHubTestTask.Add(ActivateRandomGrainsTest());
+        _checkboxHubTestTask.Add(ActivateRandomGrainsTest());
+        _checkboxHubTestTask.Add(ActivateRandomGrainsTest());
+        _checkboxHubTestTask.Add(ActivateRandomGrainsTest());
+        _checkboxHubTestTask.Add(ActivateRandomGrainsTest());
+        _checkboxHubTestTask.Add(ActivateRandomGrainsTest());
+        _checkboxHubTestTask.Add(UpdateSameGrainTest());
+        _checkboxHubTestTask.Add(UpdateSameGrainTest());
+        _checkboxHubTestTask.Add(UpdateSameGrainTest());
+        _checkboxHubTestTask.Add(UpdateSameGrainTest());
+        _checkboxHubTestTask.Add(UpdateSameGrainTest());
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
         await _testCancellationTokenSource.CancelAsync();
-
-        if (_checkboxHubTestTask != null)
-        {
-            await _checkboxHubTestTask;
-        }
+        await Task.WhenAll(_checkboxHubTestTask);
     }
 
     #endregion
 
     #region Methods
 
-    private static string Generate256BitHex()
+    private static byte[] Generate256BitHex()
     {
         // Create a byte array to hold 32 bytes (256 bits)
         var bytes = new byte[32];
@@ -71,11 +88,10 @@ public class CheckboxHubStressService : IHostedService
             rng.GetBytes(bytes);
         }
 
-        // Convert to hex string
-        return Convert.ToHexStringLower(bytes).TrimStart('0');
+        return bytes;
     }
 
-    private async Task CheckboxHubTest()
+    private async Task ActivateRandomGrainsTest()
     {
         var connection = new HubConnectionBuilder()
             .WithUrl(new Uri(_checkboxHubUrl), options =>
@@ -94,14 +110,55 @@ public class CheckboxHubStressService : IHostedService
         {
             await connection.StartAsync();
 
-            var pageId = Generate256BitHex();
+            while (!_testCancellationTokenSource.IsCancellationRequested)
+            {
+                // Generate a random page id.
+                var pageId = Generate256BitHex();
+                var randomIndex = new Random().Next(0, 4096);
+                var randomValue = new Random().Next(0, 2);
+
+                await connection.InvokeAsync("SetCheckbox", pageId, randomIndex, randomValue);
+            }
+        }
+
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error while testing CheckboxHub.");
+        }
+
+        finally
+        {
+            await connection.DisposeAsync();
+        }
+    }
+
+    private async Task UpdateSameGrainTest()
+    {
+        var connection = new HubConnectionBuilder()
+            .WithUrl(new Uri(_checkboxHubUrl), options =>
+            {
+                options.HttpMessageHandlerFactory = _ =>
+                {
+                    var handler = new HttpClientHandler();
+                    handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+                    return handler;
+                };
+            })
+            .WithAutomaticReconnect()
+            .Build();
+
+        try
+        {
+            await connection.StartAsync();
+
+            _logger.LogInformation("Starting to update same grain {PageId}.", Convert.ToHexString(RandomPageId).TrimLeadingZeroPairs());
             while (!_testCancellationTokenSource.IsCancellationRequested)
             {
                 // Generate a random page id.
                 var randomIndex = new Random().Next(0, 4096);
                 var randomValue = new Random().Next(0, 2);
 
-                await connection.InvokeAsync("SetCheckbox", pageId, randomIndex, randomValue);
+                await connection.InvokeAsync("SetCheckbox", RandomPageId, randomIndex, randomValue);
             }
         }
 
