ebab0818e7cce72b70b1717f249fbac367bef291
Kristoffer Lindahl <kristoffer.lindahl@gmail.com>
2025-07-30 22:45:59 +0200

    Style accordion. Fix an issue where accordion did not resize if high-score heights changed.

--- InfiniteCheckboxes/ClientApp/src/utils/scroll-height-observer.directive.ts
+++ InfiniteCheckboxes/ClientApp/src/utils/scroll-height-observer.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, NgZone, OnDestroy, OnInit, Output } from '@angular/core';
 
 @Directive({
   selector: '[scrollHeightObserver]',
@@ -7,26 +7,52 @@ import { Directive, ElementRef, EventEmitter, OnDestroy, OnInit, Output } from '
 export class ScrollHeightObserverDirective implements OnInit, OnDestroy {
   @Output() scrollHeightChange = new EventEmitter<number>();
 
-  private resizeObserver: ResizeObserver;
+  private observer!: MutationObserver;
+  private resizeObserver!: ResizeObserver;
   private lastScrollHeight: number;
 
-  constructor(private elementRef: ElementRef) {
-    this.lastScrollHeight = this.elementRef.nativeElement.scrollHeight;
+  constructor(private elementRef: ElementRef, private ngZone: NgZone) {
+    this.lastScrollHeight = 0;
 
-    this.resizeObserver = new ResizeObserver(() => {
-      const newScrollHeight = this.elementRef.nativeElement.scrollHeight;
-      if (newScrollHeight !== this.lastScrollHeight) {
-        this.lastScrollHeight = newScrollHeight;
-        this.scrollHeightChange.emit(newScrollHeight);
-      }
+    // Create observers outside Angular zone
+    this.ngZone.runOutsideAngular(() => {
+      this.observer = new MutationObserver(() => this.checkScrollHeight());
+      this.resizeObserver = new ResizeObserver(() => this.checkScrollHeight());
     });
   }
 
+  private checkScrollHeight(): void {
+    const newScrollHeight = this.elementRef.nativeElement.scrollHeight;
+    if (newScrollHeight !== this.lastScrollHeight) {
+      // Only run inside Angular zone when we need to emit the event
+      this.ngZone.run(() => {
+        this.lastScrollHeight = newScrollHeight;
+        this.scrollHeightChange.emit(newScrollHeight);
+      });
+    }
+  }
+
   ngOnInit() {
-    this.resizeObserver.observe(this.elementRef.nativeElement);
+    // Initialize observers outside Angular zone
+    this.ngZone.runOutsideAngular(() => {
+      this.lastScrollHeight = this.elementRef.nativeElement.scrollHeight;
+      this.ngZone.run(() => {
+        this.scrollHeightChange.emit(this.lastScrollHeight);
+      });
+
+      this.observer.observe(this.elementRef.nativeElement, {
+        attributes: true,
+        childList: true,
+        subtree: true,
+        characterData: true
+      });
+
+      this.resizeObserver.observe(this.elementRef.nativeElement);
+    });
   }
 
   ngOnDestroy() {
+    this.observer.disconnect();
     this.resizeObserver.disconnect();
   }
 }
